daff0f0c42c430c7edd52dbf74314f0b10f6d5b4
Matheus Vaz <matheus.vaz@guidance.dev>
2024-01-29 09:52:17 -0300

    feat: adds tests

---
+++ atividade13/exemplo/analisador_lexico.h
@@ -0,0 +1,10 @@
+#define MAIS      1
+#define MENOS     2 
+#define MULT      3 
+#define DIV       4 
+#define NUM       5 
+#define ID        6 
+#define ATRIB     7
+#define PV        8
+#define ABREPAR   9 
+#define FECHAPAR 10
